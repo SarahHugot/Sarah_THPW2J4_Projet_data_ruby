a73bd7f524cbfa8118cbc3ab6f948fd34eaca131
SarahHugot <sarah.hugot.pro@gmail.com>
2020-04-09 18:59:23 +0200

    projet terminé

---
+++ lib/journaliste.rb
@@ -0,0 +1,175 @@
+def liste 
+
+	journaliste = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+	return journaliste
+end
+
+def num_in_handle 
+	journaliste = liste
+	taille = journaliste.size
+	count = 0
+
+	for i in (0..taille)
+		 
+
+		j = "#{journaliste[i]}"
+
+		for k in (1..j.size)
+			letter = j[k]
+			suivant = j[k+1]
+			if  letter == "0" || letter == "1"|| letter == "2"|| letter == "3" || letter == "4" || letter == "5" || letter == "6" || letter == "7" || letter == "8" || letter == "9" 
+				count += 1 
+				if suivant == "0" || suivant == "1" || suivant == "2" || suivant == "3" || suivant == "4" || suivant == "5" || suivant == "6" || suivant == "7" || suivant == "8" || suivant == "9"
+				
+				 	count -= 1
+
+				 end
+
+			end
+
+		end
+	end
+
+
+	return count 
+end
+
+
+
+def aude_handle 
+	journaliste = liste 
+	taille = journaliste.size
+	count = 0 
+
+
+	for i in (0..taille)
+
+		j = "#{journaliste[i]}"
+		j.downcase
+
+		for k in (1..j.size)
+			if j[k] == "a" && j[k+1] == "u" && j[k+2] == "d" && j[k+3] == "e"
+				count += 1 
+			end 
+
+		end
+	end
+	
+	return count
+end
+
+
+
+def first_maj_handle
+	journaliste = liste 
+	taille = journaliste.size
+	count = 0 
+
+	for i in (0..taille)
+
+		j = "#{journaliste[i]}"
+		
+
+		if j[1] == "A" ||  j[1] == "B" || j[1] == "C" || j[1] == "D" || j[1] == "E" || j[1] == "F" || j[1] == "G" || j[1] == "H" || j[1] == "I" || j[1] == "J" || j[1] == "K" || j[1]== "M" || j[1] == "N" || j[1] == "O" || j[1] == "P" || j[1] == "Q" || j[1] == "R" || j[1] == "S" || j[1] == "T" || j[1] == "U" || j[1] == "V" || j[1] == "W" || j[1] == "Y" || j[1] == "Z"
+
+			count += 1
+
+		end
+	end
+
+
+	return count
+
+end
+
+def maj_handle
+	journaliste = liste 
+	taille = journaliste.size
+	count = 0 
+	for i in (0..taille)
+
+		if (/@["A-Z"]/.match("#{journaliste[i]}"))
+			count += 1
+		end
+	end
+	return count 
+
+end
+
+def underscore 
+	journaliste = liste
+	count = 0
+	journaliste.each{ |x| count += x.scan("_").length}
+
+	return count
+end
+
+underscore
+
+def tri_alpla
+
+	journaliste = liste 
+	a = journaliste.sort_by{|i|i.downcase}
+
+	return a
+
+end
+
+def shortest
+
+	journaliste = liste
+	b =  journaliste.sort_by{|i| i.length }
+	liste = []
+	50.times do |j|
+		liste << b[j]
+	end
+
+	return liste
+
+end
+
+
+def epenser 
+
+	journaliste  = liste
+	index = 0
+	taille = journaliste.size
+
+	taille.times do |i|
+
+		if journaliste[i] == "@epenser"
+			a = i
+
+		end
+	end
+	
+	return index
+end		
+
+def perform 
+	f = liste 
+	puts " il y a #{f.size} journaliste"
+	puts "Il y a #{num_in_handle} journalistes qui ont un numéro dans son @"
+	puts "Il y a #{aude_handle} journalistes qui ont aude dans son @"
+	puts "Il y a #{first_maj_handle} journalistes qui ont une majuscule au debut de leurs @ "
+	puts "Il y a #{maj_handle} journalistes qui ont une majuscule dans leurs @"
+	puts "Il y a #{underscore} journalistes qui ont un underscore"
+	puts "Voici les journalistes dans l'odre alphabétique :  #{tri_alpla}"
+	puts "voici les 50 premiers plus courts @ #{shortest}"
+	puts "epenser est à la position #{epenser} ! "
+
+
+
+end 
+
+perform 
+		
+
+
+
+
+
+
+
+
+
